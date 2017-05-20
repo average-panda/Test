b7a77ec12bdbedde620b1b5ac2dcff138210ea13
average-panda <edy.tlmch@gmail.com>
2017-05-20 18:24:13 +0200

    Update test1.py

--- test1/test1.py
+++ test1/test1.py
@@ -23,17 +23,18 @@ class test:
         await self.bot.say("I can do stuff!")
         
     @commands.command()
-    async def embed(self):
+    async def mbotcoms(self):
         """This does stuff!"""
 
-        Tembed = discord.Embed(title="**Music Bot Controls**", color=discord.Colour(0xffbe00), description="Please refer to the following list of commands for the music bot:")
+        Tembed = discord.Embed(title="🎶 MUSIC BOT CONTROLS 🎶", colour=discord.Colour(0xffbe00), description="`Please refer to the following list of commands for the music bot:`\n")
 
         Tembed.set_author(name="※ VOICE OF KEK ※", icon_url="https://cdn.discordapp.com/app-icons/284962616361877507/0a82d11f51b37ca2d500eff675a41b51.jpg")
-        Tembed.set_footer(text=":notes:※ VOICE OF KEK ※ Music Bot v0.7 | Updated May 20th, 2017", icon_url="https://cdn.discordapp.com/app-icons/284962616361877507/0a82d11f51b37ca2d500eff675a41b51.jpg")
-        Tembed.add_field(name="Music:", value="*`(these controls are available to everyone)`*\n```fix\n*nowplaying = shows the song that is currently playing``````fix\n*play <title|URL|subcommand> = plays the provided song``````fix\n*playlists = shows the available playlists``````fix\n*queue [pagenum] = shows the current queue``````fix\n*remove <position|ALL> = removes a song from the queue``````fix\n*shuffle = shuffles songs you have added``````fix\n*skip = votes to skip the current song````search commands` ```fix\n*search <query> = searches Youtube for a provided query``````fix\n*scsearch <query> = searches Soundcloud for a provided query```")
-        Tembed.add_field(name="DJ:", value="*`(these controls are available to ❖ LORDS OF KEK ❖)`*```fix\n*forceskip = skips the current song``````fix\n*skipto <position> = skips to the specified song``````fix\n*stop = stops the current song and clears the queue``````fix\n*volume [0-150] = sets or shows volume```")
-        Tembed.add_field(name="🙄", value="an informative error should show up, and this view will remain as-is until all issues are fixed")
-        Tembed.add_field(name="<:thonkang:219069250692841473>", value="???")
+        Tembed.set_footer(text="🎧 ※ VOICE OF KEK ※ Music Bot v0.7 | Updated May 20th, 2017")
+
+        Tembed.add_field(name="Music:", value="*`❕ these controls are available to everyone`*\n```fix\n*nowplaying = shows the song that is currently playing``````fix\n*play <title|URL|subcommand> = plays the provided song``````fix\n*playlists = shows the available playlists``````fix\n*queue [pagenum] = shows the current queue``````fix\n*remove <position|ALL> = removes a song from the queue``````fix\n*shuffle = shuffles songs you have added``````fix\n*skip = votes to skip the current song````search commands` ```fix\n*search <query> = searches Youtube for a provided query``````fix\n*scsearch <query> = searches Soundcloud for a provided query```")
+        Tembed.add_field(name="DJ:", value="*`❕ these controls are available to ❖ LORDS OF KEK ❖`*```fix\n*forceskip = skips the current song``````fix\n*skipto <position> = skips to the specified song``````fix\n*stop = stops the current song and clears the queue``````fix\n*volume [0-150] = sets or shows volume```")
+        Tembed.add_field(name="_______", value="\n❔ For additional information type `*help` or contact an admin.")
+
 
         #Your code will go here
         await self.bot.say(embed=Tembed)
