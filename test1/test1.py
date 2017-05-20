135fffbd2b1ed9224111311062d6d266c0d907a1
average-panda <edy.tlmch@gmail.com>
2017-05-20 17:39:53 +0200

    Update test1.py

--- test1/test1.py
+++ test1/test1.py
@@ -31,7 +31,7 @@ class test:
         Tembed.set_image(url="https://cdn.discordapp.com/embed/avatars/0.png")
         Tembed.set_author(name="※ VOICE OF KEK ※", icon_url="https://cdn.discordapp.com/app-icons/284962616361877507/0a82d11f51b37ca2d500eff675a41b51.jpg")
         Tembed.set_footer(text="※ VOICE OF KEK ※ Music Bot v0.7 | Updated May 20th, 2017", icon_url="https://cdn.discordapp.com/app-icons/284962616361877507/0a82d11f51b37ca2d500eff675a41b51.jpg")
-        Tembed.add_field(name="Music:", value="```fix\n*nowplaying = Shows the song that is currently playing```")
+        Tembed.add_field(name="Music:", value="*(these controls are available to everyone)*\n\n```fix\n*nowplaying = Shows the song that is currently playing``````fix\n*play <title|URL|subcommand> = plays the provided song``````fix\n*playlists = shows the available playlists``````fix\n*queue [pagenum] = shows the current queue``````fix\n*remove <position|ALL> = removes a song from the queue``````fix\n*play <title|URL|subcommand> = plays the provided song``````fix\n*play <title|URL|subcommand> = plays the provided song```")
         Tembed.add_field(name="DJ:", value="```fix\n*nowplaying = Shows the song that is currently playing```")
         Tembed.add_field(name="🙄", value="an informative error should show up, and this view will remain as-is until all issues are fixed")
         Tembed.add_field(name="<:thonkang:219069250692841473>", value="???")
