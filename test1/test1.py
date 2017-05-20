dd0d30bb21436fb042213863dfbe66316403665b
average-panda <edy.tlmch@gmail.com>
2017-05-20 18:05:07 +0200

    Update test1.py

--- test1/test1.py
+++ test1/test1.py
@@ -28,7 +28,6 @@ class test:
 
         Tembed = discord.Embed(title="**Music Bot Controls**", color=discord.Colour(0xffbe00), description="Please refer to the following list of commands for the music bot:")
 
-        Tembed.set_image(url="https://cdn.discordapp.com/embed/avatars/0.png")
         Tembed.set_author(name="※ VOICE OF KEK ※", icon_url="https://cdn.discordapp.com/app-icons/284962616361877507/0a82d11f51b37ca2d500eff675a41b51.jpg")
         Tembed.set_footer(text=":notes:※ VOICE OF KEK ※ Music Bot v0.7 | Updated May 20th, 2017", icon_url="https://cdn.discordapp.com/app-icons/284962616361877507/0a82d11f51b37ca2d500eff675a41b51.jpg")
         Tembed.add_field(name="Music:", value="*`(these controls are available to everyone)`*\n```fix\n*nowplaying = shows the song that is currently playing``````fix\n*play <title|URL|subcommand> = plays the provided song``````fix\n*playlists = shows the available playlists``````fix\n*queue [pagenum] = shows the current queue``````fix\n*remove <position|ALL> = removes a song from the queue``````fix\n*shuffle = shuffles songs you have added``````fix\n*skip = votes to skip the current song````search commands` ```fix\n*search <query> = searches Youtube for a provided query``````fix\n*scsearch <query> = searches Soundcloud for a provided query```")
