d0ee773157aa3f0da0b08938e550df39bf2818ba
average-panda <edy.tlmch@gmail.com>
2017-05-31 11:37:08 +0200

    Update cd.py

--- cd/cd.py
+++ cd/cd.py
@@ -11,13 +11,13 @@ class cd:
     @commands.command()
     async def cd(self):
         """This does stuff!"""
-        seconds = 15
+        seconds = 115
         secondint = int(seconds)
         message = "TITLE TITLE TITLE\ntest test test\ntest test test\n"
         tbody = await self.bot.say(message)
         while secondint > 0:
             mins, secs = divmod(secondint, 60)
-            timeformat = '{:02d}:{:02d}'.format(mins, secs)
+            timeformat = '{:02d}m {:02d}s'.format(mins, secs)
             await self.bot.edit_message(tbody, new_content=("```css" + message + "\nTIME: " + timeformat + "```"))
             await asyncio.sleep(1)
             secondint = secondint - 1
