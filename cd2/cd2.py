6040c65a1b5073515741edad3344d535e9089c1e
average-panda <edy.tlmch@gmail.com>
2017-05-31 14:15:30 +0200

    Update cd2.py

--- cd2/cd2.py
+++ cd2/cd2.py
@@ -2,14 +2,14 @@ import discord
 from discord.ext import commands
 import asyncio
 
-class cd2:
+class cd:
     """Countdown test"""
 
     def __init__(self, bot):
         self.bot = bot
         
     @commands.command()
-    async def cd2(self):
+    async def cd(self):
         """This does stuff!"""
         seconds = 75
         secondint = int(seconds)
@@ -19,14 +19,9 @@ class cd2:
             mins, secs = divmod(secondint, 60)
             timeformatm = '{:02d}:{:02d}'.format(mins, secs)
             if mins >= 1:
-                embed=discord.Embed(title="Game description:", description="Destiny 2 is an action shooter that takes you on an epic journey across the solar system. In the cinematic campaign, you’ll enter a world filled with compelling characters and fight to take back our home. Battle alongside friends with multiple cooperative modes or challenge others in intense multiplayer matches.", color=0x4787e7)
-                embed.set_author(name="🎁 GIVEAWAY - Destiny 2 🎁", url='https://www.destinythegame.com/home')
-                embed.set_thumbnail(url='http://www.product-reviews.net/wp-content/uploads/destiny-2-logo.jpg')
-                embed.add_field(name="How to enter:", value="🎁🎁🎁🎁", inline=False)
-                embed.set_footer(text="Give away | 31 May 2017")
                 await asyncio.sleep(5)
                 secondint = secondint - 5
-                await self.bot.edit_message(tbody, new_content=(embed=embed))
+                await self.bot.edit_message(tbody, new_content=(message + "\nTime remaining:```glsl\n- " + timeformatm + "```"))
             else:
                 await asyncio.sleep(1)
                 secondint = secondint - 1
@@ -35,5 +30,5 @@ class cd2:
         await self.bot.say("```css" + "\nDONE: {0}```".format(secondint))
 
 def setup(bot):
-    n = cd2(bot)
+    n = cd(bot)
     bot.add_cog(n)
