0a067a3689bb1d7a3d81c9011c811ea2b3613402
average-panda <edy.tlmch@gmail.com>
2017-05-31 11:50:44 +0200

    Update cd.py

--- cd/cd.py
+++ cd/cd.py
@@ -18,7 +18,7 @@ class cd:
         while secondint > 0:
             mins, secs = divmod(secondint, 60)
             timeformatm = '*{:02d}* minutes remaining... {:02d}s'.format(mins, secs)
-            await self.bot.edit_message(tbody, new_content=("```css" + message + "\nTIME: " + timeformatm + "```"))
+            await self.bot.edit_message(tbody, new_content=("```glsl" + message + "\nTIME: *" + timeformatm + "*```"))
             await asyncio.sleep(1)
             secondint = secondint - 1
         await self.bot.say("```css" + "\nDONE: {0}```".format(secondint))
