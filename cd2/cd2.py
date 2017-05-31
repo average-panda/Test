ce9ce875d3d7a579a00c560f64af3f531d1c18eb
average-panda <edy.tlmch@gmail.com>
2017-05-31 12:35:00 +0200

    Update cd2.py

--- cd2/cd2.py
+++ cd2/cd2.py
@@ -18,16 +18,9 @@ class cd:
         while secondint > 0:
             mins, secs = divmod(secondint, 60)
             timeformatm = '{:02d}:{:02d}s'.format(mins, secs)
-            if mins >= 1
-                await self.bot.edit_message(tbody, new_content=(message + "\nTime remaining:```glsl\n- " + timeformatm + "```"))
-                await asyncio.sleep(5)
-                secondint = secondint - 5
-            break
-            if mins < 1
-                await self.bot.edit_message(tbody, new_content=(message + "\nTime remaining:```diff\n- " + timeformatm + "```"))
-                await asyncio.sleep(1)
-                secondint = secondint - 1
-            break
+            await self.bot.edit_message(tbody, new_content=(message + "\nTime remaining:```diff\n- " + timeformatm + "```"))
+            await asyncio.sleep(5)
+            secondint = secondint - 5
         await self.bot.say("```css" + "\nDONE: {0}```".format(secondint))
 
 def setup(bot):
