93bbfecdded1d2c4e4ba7887035c5fa0b5947146
average-panda <edy.tlmch@gmail.com>
2017-05-31 14:12:51 +0200

    Update cd2.py

--- cd2/cd2.py
+++ cd2/cd2.py
@@ -19,9 +19,14 @@ class cd2:
             mins, secs = divmod(secondint, 60)
             timeformatm = '{:02d}:{:02d}'.format(mins, secs)
             if mins >= 1:
+                embed=discord.Embed(title="Game description:", description="Destiny 2 is an action shooter that takes you on an epic journey across the solar system. In the cinematic campaign, you’ll enter a world filled with compelling characters and fight to take back our home. Battle alongside friends with multiple cooperative modes or challenge others in intense multiplayer matches.", color=0x4787e7)
+                embed.set_author(name="🎁 GIVEAWAY - Destiny 2 🎁", url='https://www.destinythegame.com/home')
+                embed.set_thumbnail(url='http://www.product-reviews.net/wp-content/uploads/destiny-2-logo.jpg')
+                embed.add_field(name="How to enter:", value="🎁🎁🎁🎁", inline=False)
+                embed.set_footer(text="Give away | 31 May 2017")
                 await asyncio.sleep(5)
                 secondint = secondint - 5
-                await self.bot.edit_message(tbody, new_content=(message + "\nTime remaining:```glsl\n- " + timeformatm + "```"))
+                await self.bot.edit_message(tbody, new_content=(embed=embed))
             else:
                 await asyncio.sleep(1)
                 secondint = secondint - 1
