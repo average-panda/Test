c51e921b98a6a3ee76389b07c17d6412939cb3f0
average-panda <edy.tlmch@gmail.com>
2017-05-31 12:11:37 +0200

    Update cd.py

--- cd/cd.py
+++ cd/cd.py
@@ -13,18 +13,18 @@ class cd:
         """This does stuff!"""
         seconds = 75
         secondint = int(seconds)
-        message = "```TITLE TITLE TITLE\ntest test test\ntest test test\n"
+        message = "TITLE TITLE TITLE\ntest test test\ntest test test\n"
         tbody = await self.bot.say(message)
         while secondint > 0:
             mins, secs = divmod(secondint, 60)
             timeformatm = '{:02d}:{:02d}s'.format(mins, secs)
             if mins >= 1
-                await self.bot.edit_message(tbody, new_content=(message + "\nTime remaining:\n- " + timeformatm + "```"))
+                await self.bot.edit_message(tbody, new_content=(message + "\nTime remaining:```glsl\n- " + timeformatm + "```"))
                 await asyncio.sleep(5)
                 secondint = secondint - 5
             break
             if mins < 1
-                await self.bot.edit_message(tbody, new_content=(message + "\nTime remaining:\n- " + timeformatm + "```"))
+                await self.bot.edit_message(tbody, new_content=(message + "\nTime remaining:```diff\n- " + timeformatm + "```"))
                 await asyncio.sleep(1)
                 secondint = secondint - 1
             break
