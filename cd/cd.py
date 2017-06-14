800281dc136230f677625f74ec7972cb569bf683
average-panda <edy.tlmch@gmail.com>
2017-06-14 09:20:17 +0200

    Create cd.py

--- cd/cd.py
+++ cd/cd.py
@@ -13,14 +13,14 @@ class cd:
         """This does stuff!"""
         seconds = 75
         secondint = int(seconds)
-        message = discord.Embed(title="TEST TEST", description="test test test", color=0x0080ff)
+        message = discord.Embed(title="TEST TEST", description="test test test", color=0x85d9e9)
         message.add_field(name="time here TITLE", value="time here", inline=False)
         tbody = await self.bot.say(embed=message)
         while secondint > 0:
             mins, secs = divmod(secondint, 60)
             timeformatm = '{:02d}:{:02d}'.format(mins, secs)
             if secondint > 60:
-                message2 = discord.Embed(title="TEST TEST", description="test test test", color=0x0080ff)
+                message2 = discord.Embed(title="TEST TEST", description="test test test", color=0x85d9e9)
                 message2.add_field(name="Time remaining:", value="```glsl\n- " + timeformatm + "```", inline=True)
                 await self.bot.edit_message(tbody, embed=(message2))
                 await asyncio.sleep(5)
@@ -38,8 +38,8 @@ class cd:
                 await asyncio.sleep(1)
                 secondint = secondint - 1
 
-        message2 = discord.Embed(title="TEST TEST", description="test test test", color=0xff0000)
-        message2.add_field(name="Giveaway complete", value="```fix\nWinner = " + "-winner- ```", inline=False)
+        message2 = discord.Embed(title="TEST TEST", description="test test test", color=0xb6b6b6)
+        message2.add_field(name="Giveaway complete", value="```fix\nWinner = -winner- ```", inline=False)
         await self.bot.edit_message(tbody, embed=(message2))
 
 def setup(bot):
