9f858a1220317a00533beb2f6bf3355b6897d010
average-panda <edy.tlmch@gmail.com>
2017-09-16 07:36:05 +0200

    Update CRYPTO.py

--- CRYPTO/CRYPTO.py
+++ CRYPTO/CRYPTO.py
@@ -20,11 +20,37 @@ class CRYPTO:
                     return currency["CurrencyLong"]
               
         return None
+    
+    def getTickerData(pair):
+        pair = pair.replace("_", "-")
+        url = "https://bittrex.com/api/v1.1/public/getmarketsummary?market=" + pair
+
+        with urllib.request.urlopen(url) as url:
+            ticker = json.loads(url.read().decode())
+            if ticker["success"] == True:
+                return ticker["result"]
+
+        return None
         
     @commands.command()
     async def CRYPTO(self):
         """This does stuff!"""
-        await self.bot.say("Test")
+        ticker = ticker[0]
+        pair = ticker["MarketName"]
+        coin = getReadableCoinName(pair.split("-")[1])
+        header = "**" + coin + " (" + pair.replace("-", "_") + ") - Bittrex**\n"
+        seperator = "-----------------------\n"
+        price = "Current Price: `" + "{:.8f}".format(ticker["Last"]) + "`\n"
+        high = "24hr High: `" + "{:.8f}".format(ticker["High"]) + "`\n"
+        low = "24hr Low: `" + "{:.8f}".format(ticker["Low"]) + "`\n"
+        volume = "24hr Volume: `" + "{:.8f}".format(ticker["BaseVolume"]) + "`\n"
+
+        changeNum = round(((ticker["Last"] - ticker["PrevDay"]) / ticker["PrevDay"]) * 100, 2)
+        sign = "+" if changeNum > 0 else ""
+        change = "Percent Change: ```diff\n" + sign + str(changeNum) + "%\n```"
+
+        message = header + seperator + price + volume + high + low + change
+        await self.bot.say(message)
 
 def setup(bot):
     n = CRYPTO(bot)
