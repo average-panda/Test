17836150fccfbfbbabe6e1e840003c94d288daed
average-panda <edy.tlmch@gmail.com>
2017-10-21 13:51:13 +0200

    Update test1.py

--- test1/test1.py
+++ test1/test1.py
@@ -15,10 +15,10 @@ class test:
         Tembed = discord.Embed(title="🎶 MUSIC BOT CONTROLS 🎶", colour=discord.Colour(0xffbe00), description="Please refer to the following list of commands for the music bot:\n")
 
         Tembed.set_author(name="※ VOICE OF KEK ※", icon_url="https://cdn.discordapp.com/app-icons/284962616361877507/0a82d11f51b37ca2d500eff675a41b51.jpg")
-        Tembed.set_footer(text="🎧 ※ VOICE OF KEK ※ | Music Bot v0.0.5 | Updated May 20th, 2017")
+        Tembed.set_footer(text="🎧 ※ VOICE OF KEK ※ | Music Bot v0.1.0 | Updated October 21st, 2017")
 
         Tembed.add_field(name="Music:", value="*`⚪ these controls are available to everyone`*\n```fix\n*nowplaying = shows the song that is currently playing``````fix\n*play <title|URL|subcommand> = plays the provided song``````fix\n*playlists = shows the available playlists``````fix\n*queue [pagenum] = shows the current queue``````fix\n*remove <position|ALL> = removes a song from the queue``````fix\n*shuffle = shuffles songs you have added``````fix\n*skip = votes to skip the current song```\n`search commands` ```fix\n*search <query> = searches Youtube for a provided query``````fix\n*scsearch <query> = searches Soundcloud for a provided query```")
-        Tembed.add_field(name="DJ:", value="*`⚪ these controls are available to ❖ LORDS OF KEK ❖`*```fix\n*forceskip = skips the current song``````fix\n*skipto <position> = skips to the specified song``````fix\n*stop = stops the current song and clears the queue``````fix\n*volume [0-150] = sets or shows volume```\n\n\n")
+        Tembed.add_field(name="DJ:", value="*`⚪ these controls are available to ❖ LORDS OF KEK ❖`*```fix\n*pause = pauses the player (The player remains paused until a DJ or Admin uses the play command)``````fix\n*forceskip = skips the current song``````fix\n*skipto <position> = skips to the specified song``````fix\n*stop = stops the current song and clears the queue``````fix\n*volume [0-150] = sets or shows volume```\n\n\n")
 
         Tembed.add_field(name="`❔`", value="For additional information type `*help`")
         Tembed.add_field(name="`⚠️`", value="If you are experiencing issues with the\nmusic bot, or if the bot is unavailable,\ncontact `average.panda`")
